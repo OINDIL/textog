ac82edf2e333305f25b6896d41badc3b1da86b8a
oindil <oindilgolder303@gmail.com>
2023-12-04 23:02:43 +0530

    fixed some bugs and changes in design

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,17 +3,13 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <>
-      <nav className={`navbar navbar-expand-lg text-${props.mode === 'dark' ? 'light' : 'dark'} bg-${props.mode}`}>
-        <div className="container-fluid">
-          <a className={`text-${props.mode === 'dark' ? 'light' : 'dark'} navbar-brand`} href='/'><b>TextFormatter <span style={{color:'red'}}>&</span> Password Generator</b></a>
-          <div className="form-check-reverse form-check form-switch" >
+      <div className="nav">
+        <p>Text.<span>dev</span></p>
+        <div className="form-check-reverse form-check form-switch" >
             <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.togglefunc} />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault"><i className="ri-moon-fill"></i></label>
           </div>
-        </div>
-
-
-      </nav>
+      </div>
     </>
   )
 }
--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -13,6 +13,10 @@ export default function TextArea(props) {
         }
         return text.split(" ").length - count
     }
+    function noOfChars(){
+        const stringWithoutSpaces = text.replace(/\s/g, '');
+        return stringWithoutSpaces.length;
+    }
     function textUpperCase() {
         setText(text.toUpperCase())
         props.showAlert("Text converted to UPPERCASE", "success")
@@ -73,24 +77,24 @@ export default function TextArea(props) {
             <div className="mb-3">
                 <label htmlFor="myBox" className="form-label my-3"><span style={{ fontSize: '25px' }}>{props.title}</span></label>
                 <textarea id="myBox" rows="10" className="form-control" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'rgb(48, 49, 52)', color: props.mode === 'light' ? 'black' : 'white' }} value={text} onChange={onChangeHandler}></textarea>
-                    <div className='my-2'>
+                <div id="buttons">
                     <button className="btn btn-danger" onClick={generatePass}>Generate Secure password</button>
-                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'} my-2`} disabled={text.length === 0} onClick={textUpperCase}>To Uppercase</button>
-                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'} my-2`} disabled={text.length === 0} onClick={textLowerCase}>To Lowercase</button>
-                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'} my-2`} disabled={text.length === 0} onClick={alternateText}>aLtErNaTiNg tExT</button>
-                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'} my-2`} disabled={text.length === 0} onClick={removeExtraSpaces}>Remove Extra Spaces</button>
-                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'} my-2`} disabled={text.length === 0} onClick={copyText}><i class="ri-clipboard-line"></i></button>
-                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'} my-2`} disabled={text.length === 0}  onClick={textClear}><b><i class="ri-delete-bin-6-line"></i></b></button>
+                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'}`} disabled={text.length === 0} onClick={textUpperCase}>To Uppercase</button>
+                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'}`} disabled={text.length === 0} onClick={textLowerCase}>To Lowercase</button>
+                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'}`} disabled={text.length === 0} onClick={alternateText}>aLtErNaTiNg tExT</button>
+                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'}`} disabled={text.length === 0} onClick={removeExtraSpaces}>Remove Extra Spaces</button>
+                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'}`} disabled={text.length === 0} onClick={copyText}><i class="ri-clipboard-line"></i>copy</button>
+                    <button className={`btn btn-${props.mode === 'dark' ? 'light' : 'dark'}`} disabled={text.length === 0} onClick={textClear}><b><i class="ri-delete-bin-6-line"></i></b></button>
 
-                    </div>
+                </div>
             </div>
-            <div className='container'>
-                <h4>Info about the text</h4>
-                <div className="d-flex">
-                    <section style={{ border:`2px solid ${props.mode==='light'?'black':'white'}`, width: 34, height: 35, display: "flex", justifyContent: "center", alignItems: "center" }}>{lengthOfWord()}</section>
-                    <h5 className='mx-2 my-1'>words</h5>
-                    <section style={{ border: `2px solid ${props.mode==='light'?'black':'white'}`, width: 34, height: 35, display: "flex", justifyContent: "center", alignItems: "center" }}>{text.length}</section>
-                    <h5 className='mx-2 my-1'>characters</h5>
+            <div id="info-text" className='container'>
+                <h4 style={{ textDecoration: "underline" }}>Info about the text</h4>
+                <div id="info-section" className={`d-flex`} style={{ alignItems: "center" }}>
+                    <section style={{ border: `2px solid ${props.mode === 'light' ? 'black' : 'white'}`, padding: "10px", display: "flex", justifyContent: "center", alignItems: "center" }}>{lengthOfWord()}</section>
+                    <h5 className='mx-2 my-1'>words and</h5>
+                    <section style={{ border: `2px solid ${props.mode === 'light' ? 'black' : 'white'}`, padding: "10px", display: "flex", justifyContent: "center", alignItems: "center" }}>{noOfChars()}</section>
+                    <h5 className='mx-2 my-1'>characters.</h5>
                 </div>
             </div>
         </div>
